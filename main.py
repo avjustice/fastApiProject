b19c39a6b4a1e3c32bea09098f5400a882c3688c
avjustice <iatsenko.av@gmail.com>
2023-04-27 01:27:40 +0300

    Small fix

--- main.py
+++ main.py
@@ -9,14 +9,9 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
-#
-#
-# @app.get("/hello/{name}")
-# async def say_hello(name: str):
-#     return {"message": f"Hello {name}"}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
 @app.get("/{code}", response_model=schemas.CurrencyInfo | str)
